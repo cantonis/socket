0ace235f98da0fd58f22c21954bad9f1d905f205
Simone Cantoni <71756284+cantonis@users.noreply.github.com>
2021-03-02 23:30:55 +0100

    Calcolatrice con funzioni multiprocessing

--- client.py
+++ client.py
@@ -1,49 +1,67 @@
-#!/usr/bin/env python3
-
 import socket
+import sys
+import random
+import time
+import threading
+import multiprocessing
 
-SERVER_ADDRESS = '127.0.0.1'
+SERVER_ADDRESS = "127.0.0.1"
 SERVER_PORT = 22224
+NUM_WORKERS = 15
 
 
-def inviaComandi(socket):
-    while True:
-        try:
-            dati = input(
-                "Inserisci i dati dell'operazione (ko per terminare la connessione): ")
-        except EOFError:
-            print("\nOkay. Exit")
-            break
-        if not dati:
-            print("Non puoi inviare una stringa vuota!")
-            continue
-        if dati == 'ko':
-            print("Chiudo la connessione con il server!")
-            break
+def generaRichieste(address, port):
+    try:
+        s = socket.socket()
+        s.connect((address, port))
+        print("Connessione al server: " + address + ":" + str(port))
+    except socket.error as error:
+        print("Errore: " + error)
+        sys.exit()
 
-        dati = dati.encode()
+    comandi = ["piu", "meno", "per", "diviso"]
+    operazione = comandi[random.randint(0, len(comandi) - 1)]
+    dati = operazione + ";" + \
+        str(random.randint(1, 100)) + ";" + str(random.randint(1, 100))
+    dati = dati.encode()
+    s.send(dati)
 
-        socket.send(dati)
+    dati = s.recv(2048)
 
-        dati = socket.recv(2048)
+    if not dati:
+        print("Server non risponde. Exit")
+        sys.exit()
 
-        if not dati:
-            print("Server non risponde. Exit")
-            break
+    dati = dati.decode()
+    print("Ricevuto dal server:\n" + dati)
 
-        dati = dati.decode()
-
-        print("Ricevuto dal server:")
-        print(dati + '\n')
-
-
-def connessioneServer(address, port):
-    sock_service = socket.socket()
-    sock_service.connect((address, port))
-    print("Connesso a " + str((address, port)))
-    inviaComandi(sock_service)
-    sock_service.close()
+    dati = "ko"
+    dati = dati.encode()
+    s.send(dati)
+    s.close()
 
 
 if __name__ == "__main__":
-    connessioneServer(SERVER_ADDRESS, SERVER_PORT)
+    start = time.time()
+    for _ in range(0, NUM_WORKERS):
+        generaRichieste(SERVER_ADDRESS, SERVER_PORT)
+    end = time.time()
+    print("\nTempo di esecuzione sequenziale: " + str(end - start) + "\n")
+
+    start = time.time()
+    threads = [threading.Thread(target=generaRichieste, args=(
+        SERVER_ADDRESS, SERVER_PORT)) for _ in range(NUM_WORKERS)]
+    for t in threads:
+        t.start()
+        t.join()
+    end = time.time()
+    print("\nTempo di esecuzione multithreading: " + str(end - start) + "\n")
+
+    start = time.time()
+    processes = [multiprocessing.Process(target=generaRichieste, args=(
+        SERVER_ADDRESS, SERVER_PORT)) for _ in range(NUM_WORKERS)]
+    for p in processes:
+        p.start()
+        p.join()
+    end = time.time()
+    print("\nTempo di esecuzione multiprocessing: " + str(end - start) + "\n")
